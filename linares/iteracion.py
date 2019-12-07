d25265f7a8492418e80be2f3c84b231a17cfaca4
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-12-07 16:19:32 -0500

    se ha terminado los ejercicios de iteracion

---
+++ linares/iteracion.py
@@ -0,0 +1,88 @@
+#ejercicio1
+msj="HOLA ME LLAMO ERICK, ¿COMO ESTAS? "
+for letra in msj:
+    print(letra)
+
+#ejercicio2
+
+msj=" ¿CUANDO ME VISITAS? "
+for letra in msj:
+    print(letra)
+
+#ejercicio3
+
+msj=" ESTOY CANSADO DE TANTO TRABAJAR "
+for letra in msj:
+    print(letra)
+
+#ejercicio4
+
+msj=" TENGO QUE APROBAR TODOS LOS CURSOS "
+for letra in msj:
+    print(letra)
+
+#ejercicio5
+
+msj=" ME VISITAS CUANDO PUEDAS, PORQUE NECESITO VERTE "
+for letra in msj:
+    print(letra)
+
+#ejercicio6
+
+msj=" ELLA NO TE AMA, POR ESO SACALA DE TU VIDA "
+for letra in msj:
+    print(letra)
+
+#ejercicio7
+
+msj=" PERU TIENE UNA DE LAS MARAVILLAS DEL MUNDO "
+for letra in msj:
+    print(letra)
+
+#ejercicio8
+
+msj=" MARIA ES UNA GRAN ALUMNA DE CIVIL "
+for letra in msj:
+    print(letra)
+
+#ejercicio9
+
+msj=" MANUEL SE CASARA EL PROXIMO AÑO "
+for letra in msj:
+    print(letra)
+
+#ejercicio10
+
+msj=" KATY PERRY ES UNA DE LAS CANTANTES MAS FAMOSAS DEL MUNDO "
+for letra in msj:
+    print(letra)
+
+#ejercicio11
+
+msj=" ANDRES NO APROBO TODOS LOS CURSOS "
+for letra in msj:
+    print(letra)
+
+#ejercicio12
+
+msj=" FATIMA SE LEVANTA MUY TARDE "
+for letra in msj:
+    print(letra)
+
+#ejercicio13
+
+msj=" REBECA ES PROFESORA DE INGLES "
+for letra in msj:
+    print(letra)
+
+#ejercicio14
+
+msj=" FERNANDO ENSEÑA PROGRAMACION EN LA FACFYM "
+for letra in msj:
+    print(letra)
+
+#ejercicio15
+
+msj=" KARLA ES TUTORA EN LA ACADEMIA ELITE "
+for letra in msj:
+    print(letra)
