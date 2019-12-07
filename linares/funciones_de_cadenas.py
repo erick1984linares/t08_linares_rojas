26e0721294f67209fba00532a9ad88c0a855ff3f
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-12-07 16:19:00 -0500

    se ha terminado los ejercicios de funciones de cadenas

---
+++ linares/funciones_de_cadenas.py
@@ -0,0 +1,88 @@
+#ejercicio1
+#revisar si el texto es de numero
+aux="1500"
+print(aux.isdigit())
+
+#ejercicio2
+#Dividir cadenas
+msj= " python, java, java script "
+lista_lenguaje=msj.split(",")
+for item in lista_lenguaje:
+    print(item)
+
+#ejercicio3
+#Dividir cadenas
+msj= " ANA, LUIS, PEDRO, ERICK, CARLOS "
+lista_estudiantes=msj.split(",")
+for item in lista_estudiantes:
+    print(item)
+
+#ejercicio4
+#
+msj= " python, java, java script "
+lista_lenguaje=msj.split(",")
+for item in lista_lenguaje:
+    print(item)
+
+#ejercicio5
+#revisar si el texto es numero
+aux="120-32"
+print(aux.isdigit())
+
+#ejercicio6
+msj= "SOLO SE, QUE NADA SE"
+#Transformar el texto en minusculas
+mensaje_minuscula=msj.lower()
+print(mensaje_minuscula)
+
+#ejercicio7
+msj= "el ayer es historia, el mañana es un misterio, el hoy es un regalo, por eso se llama presente"
+#Transformar el texto en minusculas
+mensaje_mayuscula=msj.upper()
+print(mensaje_mayuscula)
+
+#ejercicio8
+msj= "elige un trabajo que ames y no tendrás que trabajar ningún día de tu vida."
+#Transformar el texto en minusculas
+mensaje_mayuscula=msj.upper()
+print(mensaje_mayuscula)
+
+#ejercicio9
+msj= "NO PERMITAS QUE ALGUIEN TE DIGA QUE ERES INCAPAZ DE HACER ALGO"
+#Transformar el texto en minusculas
+mensaje_minuscula=msj.lower()
+print(mensaje_minuscula)
+
+#ejercicio10
+#revisar si el texto es numero
+aux="192027K"
+print(aux.isdigit())
+
+#ejercicio11
+#revisar si el texto es numero
+aux="2019"
+print(aux.isdigit())
+
+#ejercicio12
+#revisar si el texto es imprimible
+msj= "TE AMO"
+print(msj.isprintable())
+
+#ejercicio13
+#revisar si el texto pertenece al ASCII
+msj= "PROGRAMACION I ES INTERESANTE"
+print(msj.isascii())
+
+#ejercicio14
+#revisar si el texto tiene caracter del alfabeto
+msj= "INGENIERIA ELECTRONICA"
+print(msj.isalpha())
+
+
+#ejercicio15
+#revisar si el texto es alfanumerico
+msj= "192027k"
+print(msj.isalnum())
+
+
+
