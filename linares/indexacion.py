0b916f9aa682583d3a4f0937413af24074c7d4d2
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-12-07 16:19:16 -0500

    se ha terminado los ejercicios de indexacion

---
+++ linares/indexacion.py
@@ -0,0 +1,97 @@
+#ejercicio1
+#              1         2         3
+#    01234567890123456789012345678901
+msj="HOY TENEMOS EXAMEN DE MATEMATICA"
+print(msj[22] + msj[23] + msj[24] + msj[25] + msj[9]) #Devuelve MATEO
+
+#ejercicio2
+#              1         2         3
+#    012345678901234567890123456789012
+msj="YO ESTUDIO INGENIERIA ELECTRONICA"
+print(msj[7] + msj[8] + msj[32] + msj[15] + msj[20]) #Devuelve DIANA
+
+#ejercicio3
+#              1         2         3
+#    012345678901234567890123456789012
+msj="ESTE AÑO FUE BUENO Y MALO PARA MI"
+print(msj[9] + msj[5] + msj[13] + msj[32] + msj[27] + msj[16]) #Devuelve FABIAN
+
+
+#ejercicio4
+#              1         2         3
+#    01234567890123456789012345678901234
+msj="JAMES ES MUY APLICADO EN EL COLEGIO"
+print(msj[14] + msj[22] + msj[14] + msj[22]) #Devuelve PEPE
+
+
+#ejercicio5
+#              1         2         3
+#    012345678901234567890123456789012345678
+msj="NOEMI COCINA MUY RICO, DEBERIA SER CHEF"
+print(msj[35] + msj[37] + msj[31] + msj[11] + msj[33]) #Devuelve CESAR
+
+
+#ejercicio6
+#              1         2         3
+#    01234567890123456789012345678901234
+msj="NEYMAR ES UN GRAN JUGADOR DE FUTBOL"
+print(msj[5] + msj[23] + msj[32] + msj[1] + msj[24] + msj[31] + msj[33]) #Devuelve ROBERTO
+
+
+#ejercicio7
+#              1         2         3
+#    0123456789012345678901234567890123456
+msj="LA UNIVERSIDAD SE LICENCIARA ESTE AÑO"
+print(msj[11] + msj[10] + msj[12] + msj[4] + msj[34]) #Devuelve DIANA
+
+
+#ejercicio8
+#              1         2         3         4
+#    012345678901234567890123456789012345678901234567
+msj="PERU ES UNO DE LOS PAISES MAS HERMOSOS DEL MUNDO"
+print(msj[0] + msj[13] + msj[39] + msj[32] + msj[47]) #Devuelve PEDRO
+
+
+#ejercicio9
+#              1
+#    012345678901234567
+msj="LA UNPRG ES GRANDE"
+print(msj[0] + msj[1] + msj[3] + msj[13] + msj[14]) #Devuelve LAURA
+
+
+#ejercicio10
+#              1         2         3         4
+#    0123456789012345678901234567890123456789012
+msj="PYTHON Y JAVA SON LENGUAJES DE PROGRAMACION"
+print(msj[9] + msj[10] + msj[21] + msj[22] + msj[23] + msj[35]) #Devuelve JAGUAR
+
+#ejercicio11
+#              1         2         3
+#    0123456789012345678901234567890
+msj="LA FACFYM ESTUVO DE ANIVERSARIO"
+print(msj[3] + msj[4] + msj[12] + msj[29] + msj[8] + msj[1]) #Devuelve FATIMA
+
+#ejercicio12
+#              1         2         3
+#    01234567890123456789012345678901
+msj="MAÑANA ES FIN DE SEMANA, POR FIN"
+print(msj[17] + msj[1] + msj[27] + msj[1]) #Devuelve SARA
+
+#ejercicio13
+#              1         2         3
+#    0123456789012345678901234567890123456
+msj="EL CANCER ES UNA ENFERMEDAD MUY GRAVE"
+print(msj[19] + msj[20] + msj[21] + msj[14] + msj[15] + msj[14] + msj[24] + msj[25]) #Devuelve FERNANDA
+
+#ejercicio14
+#              1         2         3         4         5
+#    012345678901234567890123456789012345678901234567890123
+msj="LAURA ES DOCTORA EN EL HOSPITAL REGIONAL DE LAMBAYEQUE"
+print(msj[9] + msj[1] + msj[14] + msj[35] + msj[24]) #Devuelve DARIO
+
+#ejercicio15
+#              1         2         3
+#    0123456789012345678901234567890
+msj="MI COMPUTADORA ESTUVO CON VIRUS"
+print(msj[27] + msj[26] + msj[13] + msj[24] + msj[9]) #Devuelve IVANA
+
