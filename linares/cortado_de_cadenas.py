92517ffa5412095888a9115e9d77c5eb34a1bcc6
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-12-07 16:18:27 -0500

    se ha terminado los ejercicios de cortado de cadenas

---
+++ linares/cortado_de_cadenas.py
@@ -0,0 +1,104 @@
+#ejercicio1
+#              1         2         3
+#    01234567890123456789012345678901
+msj="HOY TENEMOS EXAMEN DE MATEMATICA"
+print(msj[22:]) #Devuelve MATEMATICA
+print(msj[10:3:-1]) #Devuelve SOMENET
+
+#ejercicio2
+#              1         2         3
+#    012345678901234567890123456789012
+msj="YO ESTUDIO INGENIERIA ELECTRONICA"
+print(msj[11:]) #Devuelve INGENIERIA ELECTRONICA
+print(msj[9:2:-1]) #Devuelve OIDUTSE
+
+#ejercicio3
+#              1         2         3
+#    012345678901234567890123456789012
+msj="ESTE AÑO FUE BUENO Y MALO PARA MI"
+print(msj[29:25:-1]) #Devuelve ARAP
+print(msj[5:8] + " " + msj[13:18]) #Devuelve AÑO BUENO
+
+#ejercicio4
+#              1         2         3
+#    01234567890123456789012345678901234
+msj="JAMES ES MUY APLICADO EN EL COLEGIO"
+print(msj[4:0:-1]) #Devuelve SEMA
+print(msj[20:12:-1]) #Devuelve ODACILPA
+
+#ejercicio5
+#              1         2         3
+#    012345678901234567890123456789012345678
+msj="NOEMI COCINA MUY RICO, DEBERIA SER CHEF"
+print(msj[23:30]) #Devuelve BEBERIA
+print(msj[6:12]) #Devuelve COCINA
+
+#ejercicio6
+#              1         2         3
+#    01234567890123456789012345678901234
+msj="NEYMAR ES UN GRAN JUGADOR DE FUTBOL"
+print(msj[29:]) #Devuelve FUTBOL
+print(msj[16:12:-1]) #Devuelve NARG
+
+#ejercicio7
+#              1         2         3
+#    0123456789012345678901234567890123456
+msj="LA UNIVERSIDAD SE LICENCIARA ESTE AÑO"
+print(msj[3:14] + " " + msj [34:]) #Devuelve UNIVERSIDAD AÑO
+print(msj[18:28]) #Devuelve LICENCIARA
+
+#ejercicio8
+#              1         2         3         4
+#    012345678901234567890123456789012345678901234567
+msj="PERU ES UNO DE LOS PAISES MAS HERMOSOS DEL MUNDO"
+print(msj[33:]) #Devuelve MUNDO
+print(msj[0:25]) #Devuelve PERU ES UNO DE LOS PAISES
+
+#ejercicio9
+#              1
+#    012345678901234567
+msj="LA UNPRG ES GRANDE"
+print(msj[7:2:-1]) #Devuelve GRPNU
+print(msj[12:16]) #Devuelve GRAN
+
+#ejercicio10
+#              1         2
+#    012345678901234567890123456
+msj="PYTHON Y JAVA SON LENGUAJES"
+print(msj[0:]) #Devuelve todo
+print(msj[0:13]) #Devuelve PYTHON Y JAVA
+
+#ejercicio11
+#              1         2         3
+#    0123456789012345678901234567890
+msj="LA FACFYM ESTUVO DE ANIVERSARIO"
+print(msj[30:2:-1]) #Devuelve OIRASREVINA ED OVUTSE MYFCAF
+print(msj[15:2:-1]) #Devuelve OVUTSE MYFCAF
+
+#ejercicio12
+#              1         2
+#    01234567890123456789012
+msj="MAÑANA ES FIN DE SEMANA"
+print(msj[22:9:-1]) #Devuelve ANAMES ED NIF
+print(msj[17:]) #Devuelve todo
+
+#ejercicio13
+#              1         2
+#    012345678901234567890123456
+msj="DADEMREFNE ANU SE RECNAC LE"
+print(msj[9:0:-1]) #Devuelve ENFERMEDA
+print(msj[23:17:-1]) #Devuelve CANCER
+
+#ejercicio14
+#              1
+#    0123456789012345
+msj="AROTCOD SE ARUAL"
+print(msj[15:11:-1]) #Devuelve LAURA
+print(msj[6:0:-1]) #Devuelve DOCTOR
+
+#ejercicio15
+#              1         2         3
+#    0123456789012345678901234567890
+msj="MI COMPUTADORA ESTUVO CON VIRUS"
+print(msj[30:2:-1]) #Devuelve SURIV NOC OVUTSE ARODATUPMOC
+print(msj[3:14] + " " + msj[21:]) #Devuelve COMPUTADORA  CON VIRUS
