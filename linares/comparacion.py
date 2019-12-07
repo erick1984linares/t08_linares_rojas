342d8644c09af191114fed48d2b38f11ac28ac49
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-12-07 16:17:11 -0500

    se ha terminado los ejercicios de comparacion

---
+++ linares/comparacion.py
@@ -0,0 +1,91 @@
+#ejercicio1
+#              1
+#    0123456789012345678
+msj="HOLA ME LLAMO ERICK"
+print( "A" < "K" )
+
+#ejercicio2
+#              1         2         3         4
+#    01234567890123456789012345678901234567890
+msj="MAÑANA HAY CLASES DE TECNICAS DE ESTUDIOS"
+print( "M" != "S" )
+
+#ejercicio3
+#              1         2
+#    0123456789012345678901234567
+msj="MI HERMANO SE LLAMA FABRICIO"
+print( "H" > "L" )
+
+#ejercicio4
+#              1         2
+#    0123456789012345678901234567
+msj="EMPEZO VERANO"
+print( "P" >= "V" )
+
+#ejercicio5
+#              1         2         3
+#    01234567890123456789012345678901234
+msj="PROGRAMACION ES UN CUROS DE CARRERA"
+print( "P" > "O" )
+
+#ejercicio6
+#              1         2
+#    0123456789012345678901234567
+msj="MI HERMANO SE LLAMA FABRICIO"
+print( "H" > "L" )
+
+#ejercicio7
+#              1         2         3
+#    012345678901234567890123456789012
+msj="ESTE AÑO FUE BUENO Y MALO PARA MI"
+print( "E" > "Ñ" )
+
+#ejercicio8
+#              1         2         3
+#    01234567890123456789012345678901234
+msj="JAMES ES MUY APLICADO EN EL COLEGIO"
+print( "J" == "L" )
+
+#ejercicio9
+#              1         2         3
+#    01234567890123456789012345678901234
+msj="NEYMAR ES UN GRAN JUGADOR DE FUTBOL"
+print( "Y" > "F" )
+
+#ejercicio10
+#              1         2         3         4
+#    012345678901234567890123456789012345678901234567
+msj="PERU ES UNO DE LOS PAISES MAS HERMOSOS DEL MUNDO"
+print( "P" < "H" )
+
+#ejercicio11
+#              1
+#    012345678901234567
+msj="LA UNPRG ES GRANDE"
+print( "G" == "E" )
+
+#ejercicio12
+#              1         2
+#    012345678901234567890123456
+msj="PYTHON Y JAVA SON LENGUAJES"
+print( "P" != "J" )
+
+#ejercicio13
+#              1         2
+#    0123456789012345678901234567
+msj="EL CANCER ES UNA ENFERMEDAD"
+print( "C" >= "F" )
+
+#ejercicio14
+#              1         2         3         4         5
+#    01234567890123456789012345678901234567890123456789012
+msj="LOS DINOSAURIOS SE EXTINGUIERON HACE MILLONES DE AÑOS"
+print( "D" > "M" )
+
+#ejercicio15
+#              1         2         3
+#    0123456789012345678901234567890123
+msj="MI COMPUTADORA ES DE MARCA SANSUMG"
+print( "E" == "G" )
+
+
