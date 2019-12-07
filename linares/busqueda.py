4716a319f07f3442217e571b1b675323a0f02030
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-12-07 16:16:42 -0500

    se ha terminado los ejercicios de busqueda

---
+++ linares/busqueda.py
@@ -0,0 +1,90 @@
+#ejercicio1
+#              1         2         3
+#    012345678901234567890123456789012
+msj="HOLA ME LLAMO ERICK, ¿COMO ESTAS?"
+print(msj.find("ERICK"))
+
+#ejercicio2
+#              1
+#    0123456789012345678
+msj="¿CUANDO ME VISITAS?"
+print(msj.find("CUANDO"))
+
+#ejercicio3
+#              1         2         3
+#    0123456789012345678901234567890
+msj="ESTOY CANSADO DE TANTO TRABAJAR"
+print(msj.index("T"))
+
+#ejercicio4
+#              1         2         3
+#    0123456789012345678901234567890123
+msj="TENGO QUE APROBAR TODOS LOS CURSOS"
+print(msj.index("A"))
+
+#ejercicio5
+#              1         2         3         4
+#    01234567890123456789012345678901234567890123456
+msj="ME VISITAS CUANDO PUEDAS, PORQUE NECESITO VERTE"
+print(msj.index("CUANDO"))
+
+#ejercicio6
+#              1         2         3         4
+#    01234567890123456789012345678901234567890
+msj="ELLA NO TE AMA, POR ESO SACALA DE TU VIDA"
+print(msj.find("AMA"))
+
+#ejercicio7
+#              1         2         3         4
+#    012345678901234567890123456789012345678901
+msj="PERU TIENE UNA DE LAS MARAVILLAS DEL MUNDO"
+print(msj.find("MUNDO"))
+
+#ejercicio8
+#              1         2         3
+#    012345678901234567890123456789012
+msj="MARIA ES UNA GRAN ALUMNA DE CIVIL"
+print(msj.find("ALUMNA"))
+
+#ejercicio9
+#              1         2         3
+#    0123456789012345678901234567890
+msj="MANUEL SE CASARA EL PROXIMO AÑO"
+print(msj.find("P"))
+
+#ejercicio10
+#              1         2         3         4         5
+#    01234567890123456789012345678901234567890123456789012345
+msj="KATY PERRY ES UNA DE LAS CANTANTES MAS FAMOSAS DEL MUNDO"
+print(msj.find("K"))
+
+#ejercicio11
+#              1         2         3
+#    012345678901234567890123456789012
+msj="ANDRES NO APROBO TODOS LOS CURSOS"
+print(msj.find("HOLA"))
+
+#ejercicio12
+#              1         2
+#    012345678901234567890123456
+msj="FATIMA SE LEVANTA MUY TARDE"
+print(msj.index("LEVANTA"))
+
+#ejercicio13
+#              1         2
+#    01234567890123456789012345678
+msj="REBECA ES PROFESORA DE INGLES"
+print(msj.find("INGLES"))
+
+#ejercicio14
+#              1         2         3         4
+#    01234567890123456789012345678901234567890
+msj="FERNANDO ENSEÑA PROGRAMACION EN LA FACFYM"
+print(msj.index("FACFYM"))
+
+#ejercicio15
+#              1         2         3
+#    012345678901234567890123456789012345
+msj="KARLA ES TUTORA EN LA ACADEMIA ELITE"
+print(msj.index("ELITE"))
+
